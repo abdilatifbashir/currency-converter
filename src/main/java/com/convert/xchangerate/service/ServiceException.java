c79470a2763f7863dad9f503bfb72a490139df4b
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 12:53:40 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/service/ServiceException.java
+++ src/main/java/com/convert/xchangerate/service/ServiceException.java
@@ -1,1 +1,15 @@
 package com.convert.xchangerate.service;
+
+public class ServiceException extends Exception {
+        public ServiceException() {
+                super();
+        }
+        public ServiceException(String message) {
+                super(message);
+        }
+        public ServiceException(String message, Throwable cause) {
+                super(message, cause);
+        }
+
+        
+}
