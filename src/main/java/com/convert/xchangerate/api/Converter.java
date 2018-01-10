23c5a2bdc777277f0958ce554abd076c013a44b3
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 17:24:10 +0300

    add src/main/java/com/convert/xchangerate/api/Converter.java

--- src/main/java/com/convert/xchangerate/api/Converter.java
+++ src/main/java/com/convert/xchangerate/api/Converter.java
@@ -8,3 +8,8 @@ import com.convert.xchangerate.endpoint.EndpointException;
 import com.convert.xchangerate.service.ServiceException;
 import com.convert.xchangerate.storage.StorageException;
 import com.convert.xchangerate.util.Currency;
+
+public interface Converter {
+
+        public BigDecimal convertCurrency(BigDecimal moneyAmount, Currency fromCurrency, Currency toCurrency) throws CurrencyNotSupportedException, JSONException, StorageException, EndpointException, ServiceException;
+}
