3324fc75c183ef394d5bac736f94367d2c3efde7
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 13:57:34 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/service/HttpService.java
+++ src/main/java/com/convert/xchangerate/service/HttpService.java
@@ -2,3 +2,7 @@ package com.convert.xchangerate.service;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+public interface HttpService {
+        public JSONObject getResponse(HttpMethods method) throws JSONException, ServiceException;
+}
