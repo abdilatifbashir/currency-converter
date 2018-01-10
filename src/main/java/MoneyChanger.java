a2527503edb94bbce4ee0b7e8e01f223416ae617
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 10:30:23 +0300

    add gradlew

--- src/main/java/MoneyChanger.java
+++ src/main/java/MoneyChanger.java
@@ -4,6 +4,9 @@ public class MoneyChanger {
     int cents = 393;
     int centsLeft = 0;
     int dollars = 0;
-    
+    int quarters = 0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennies = 0;
   }
 }
