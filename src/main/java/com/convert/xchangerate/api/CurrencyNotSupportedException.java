3e48201b8d0fa7319d9fc0ae3e1154aa4e3d5ac4
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 16:12:11 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/api/CurrencyNotSupportedException.java
+++ src/main/java/com/convert/xchangerate/api/CurrencyNotSupportedException.java
@@ -5,5 +5,7 @@ public class CurrencyNotSupportedException extends Exception{
                 super();
         }
 
-        
+        public CurrencyNotSupportedException(String message) {
+                super(message);
+        }
 }
