5a71f8c1cd28178df4c14c639919c1d613f083c0
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 11:37:30 +0300

    add gradlew

--- src/test/java/convert/ConversionTest.java
+++ src/test/java/convert/ConversionTest.java
@@ -6,3 +6,12 @@ import org.json.JSONException;
 import org.junit.BeforeClass;
 import org.testing.Assert;
 import org.testing.annotations.Test;
+
+import com.convert.xchangerate.api.CurrencyConverter;
+import com.convert.xchangerate.api.CurrencyConverterBuilder;
+import com.convert.xchangerate.api.CurrencyNotSupportedException;
+import com.convert.xchangerate.endpoint.EndpointException;
+import com.convert.xchangerate.service.ServiceException;
+import com.convert.xchangerate.storage.StorageException;
+import com.convert.xchangerate.util.Currency;
+import com.convert.xchangerate.util.Strategy;
