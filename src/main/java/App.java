2a756099d5d90ff807aee310e8ea0f91a1191e4a
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-10 09:49:31 +0300

    add root root

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+public class App {
+  public static void main(String[] args) {
+      String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
+
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
+    //Root that triggers my templates
+    get("/", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/index.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+
+  }
+}
