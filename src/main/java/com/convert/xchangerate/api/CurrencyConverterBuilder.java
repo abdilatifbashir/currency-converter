56b7c107a2b2859f54c048d98137fe04dfa6cfe6
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 16:31:24 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/api/CurrencyConverterBuilder.java
+++ src/main/java/com/convert/xchangerate/api/CurrencyConverterBuilder.java
@@ -14,5 +14,22 @@ public class CurrencyConverterBuilder {
         public CurrencyConverterBuilder() {
 
         }
-        
+
+        public CurrencyConverter buildConverter() {
+                if (_strategy == null) {
+                        throw new IllegalArgumentException("No Strategy defined to create Currency converter");
+                } else if (_strategy.equals(Strategy.YAHOO_FINANCE_FILESTORE)) {
+                        diskStore = new FileStore("yahoo");
+                        return new CurrencyConverter(diskStore, new YahooENdpoint(diskStore));
+                } else if (_strategy.equals(Strategy.CURRENCY_LAYER_FILESTORE) && _accessKey != null) {
+                        diskStore = new FileStore("currencyLayer");
+                        return new CurrencyConverter(diskStore, new CurrencyLayerEndpoint(diskStore, _accessKey));
+                }
+                throw new IllegalArgumentException("Strategy: " + _strategy + " requires accessKey for endpoint service, none provided!");
+        }
+
+        public CurrencyConverterBuilder strategy(Strategy _strategy) {
+                this._accessKey = _accessKey;
+                return this;
+        }
 }
