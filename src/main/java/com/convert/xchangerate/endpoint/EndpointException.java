52d6787577be969d02f343a5dab1364371212168
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 15:26:07 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/endpoint/EndpointException.java
+++ src/main/java/com/convert/xchangerate/endpoint/EndpointException.java
@@ -1,1 +1,11 @@
 package com.convert.xchangerate.endpoint;
+
+public class EndpointException extends Exception {
+        public EndpointException() {
+                super();
+        }
+        public EndpointException(String message) {
+                super(message);
+        }
+        
+}
