f447a3e0aee165c59f09d0b8dc01f1f7630b63c1
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 11:32:16 +0300

    add gradlew

--- src/test/java/convert/ConversionTest.java
+++ src/test/java/convert/ConversionTest.java
@@ -1,3 +1,8 @@
 package convert.xchangerate;
 
 import java.math.BigDecimal;
+
+import org.json.JSONException;
+import org.junit.BeforeClass;
+import org.testing.Assert;
+import org.testing.annotations.Test;
