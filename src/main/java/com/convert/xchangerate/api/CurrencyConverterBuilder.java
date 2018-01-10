3023e205794e251996df2b3fe356e84887466c4b
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 16:19:13 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/api/CurrencyConverterBuilder.java
+++ src/main/java/com/convert/xchangerate/api/CurrencyConverterBuilder.java
@@ -5,3 +5,9 @@ import com.convert.xchangerate.endpoint.YahooEndpoint;
 import com.convert.xchangerate.storage.DiskStore;
 import com.convert.xchangerate.storage.FileStore;
 import com.convert.xchangerate.util.Strategy;
+
+public class CurrencyConverterBuilder {
+        private String _accessKey;
+        private Strategy _strategy;
+        private DiskStore diskStore;
+}
