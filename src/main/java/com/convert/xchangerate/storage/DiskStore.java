30f89928af15d1ab79ca71ed27f1117b2f22ffd9
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 12:45:23 +0300

    add gradlew

--- src/main/java/com/convert/xchangerate/storage/DiskStore.java
+++ src/main/java/com/convert/xchangerate/storage/DiskStore.java
@@ -1,3 +1,9 @@
 package com.convert.xchangerate.storage;
 
 import org.json.JSONObject;
+
+public abstract class DiskStore {
+        public String resourceFilepath = System.getProperty("java.io.tmpdir");
+
+        
+}
