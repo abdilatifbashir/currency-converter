6fd1e2274f3d697b9be33b9b95a888c1d503fe06
Stewart ilondanga <stilondanga@gmail.com>
2018-01-11 09:07:00 +0300

    add src/main/java/com/convert/xchangerate/endpoint/CurrencyLayerEndpoint.java

--- src/main/java/com/convert/xchangerate/endpoint/CurrencyLayerEndpoint.java
+++ src/main/java/com/convert/xchangerate/endpoint/CurrencyLayerEndpoint.java
@@ -10,7 +10,7 @@ import com.convert.xchangerate.util.Currency;
 public class CurrencyLayerEndpoint extends EndpointFactory {
         public static final String BASE_URL = "http://apilayer.net/api/";
         public static final String ENDPOINT = "live";
-        private static final java.util.logging.LOGGER = java.util.logging.Logger.getLogger(CurrencyLayerEndpoint.class.getName());
+        private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(CurrencyLayerEndpoint.class.getName());
 
         public CurrencyLayerENdpoint(DiskStore diskStore, String accessKey) {
                 super(diskStore, Currency.USD, BASE_URL + ENDPOINT + "?access_key=" + accessKey);
