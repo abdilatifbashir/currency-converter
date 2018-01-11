edd02326183758f6fefdfd4d52c8e79424185861
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-11 10:31:02 +0300

    add changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,11 +26,13 @@ public class App {
   }, new VelocityTemplateEngine());
 
   //Root that triggers my templates
-  get("/home", (request, response) -> {
-  Map<String, Object> model = new HashMap<String, Object>();
-  model.put("template", "templates/home.vtl");
-  return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
+
+/*
+get("/contact-us", (request, response) -> {
+Map<String, Object> model = new HashMap<String, Object>();
+model.put("template", "templates/home.vtl");
+return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());*/
 
 
 
