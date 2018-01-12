ca4b97d1a8095540fcd5cc864afa62ea68bcca96
Stewart ilondanga <stilondanga@gmail.com>
2018-01-12 10:41:18 +0300

    add  .gradle/4.3.1/taskHistory/taskHistory.lock

--- com/convert/xchangerate/service/HttpService.java
+++
@@ -1,8 +0,0 @@
-package com.convert.xchangerate.service;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public interface HttpService {
-        public JSONObject getResponse(HttpMethods method) throws JSONException, ServiceException;
-}
